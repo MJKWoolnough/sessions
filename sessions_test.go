ce3a10494842961f5f244454ce1364823759d99f
Michael Woolnough <michael.woolnough@gmail.com>
2025-10-19 17:53:17 +0100

    add signing tests

--- sessions_test.go
+++ sessions_test.go
@@ -7,8 +7,41 @@ import (
 	"time"
 )
 
-func TestCookies(t *testing.T) {
+func TestEncode(t *testing.T) {
+	t.Parallel()
+
 	store, err := NewCookieStore([]byte("!THIS IS MY KEY!"), Expiry(time.Second))
+	if err != nil {
+		t.Fatalf("unexpected error: %s", err)
+	}
+
+	w := httptest.NewRecorder()
+
+	const myData = "MY_DATA"
+
+	store.Set(w, []byte(myData))
+
+	r := httptest.NewRequest(http.MethodGet, "http://example.com", nil)
+
+	for _, cookie := range w.Result().Cookies() {
+		r.AddCookie(cookie)
+	}
+
+	if data := store.Get(r); string(data) != myData {
+		t.Errorf("expected to read %q, got %q", myData, data)
+	}
+
+	time.Sleep(2 * time.Second)
+
+	if data := store.Get(r); data != nil {
+		t.Errorf("expected to read nil, got %q", data)
+	}
+}
+
+func TestSign(t *testing.T) {
+	t.Parallel()
+
+	store, err := NewCookieStore([]byte("!THIS IS MY KEY!"), Expiry(time.Second), Sign())
 	if err != nil {
 		t.Fatalf("unexpected error: %s", err)
 	}
